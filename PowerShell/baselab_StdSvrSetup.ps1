7ea9d3992d039d3321ded25090aa5b7af85f50d0
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2022-12-20 16:00:26 +0800

    Update baselab_StdSvrSetup.ps1
    Install standard developer software: google chrome, Notepad++, etc and copy the join domain ps1 file.

--- PowerShell/baselab_StdSvrSetup.ps1
+++ PowerShell/baselab_StdSvrSetup.ps1
@@ -6,3 +6,9 @@ choco install googlechrome -y
 choco install putty -y
 choco install notepadplusplus -y
 choco install winscp -y
+
+#Download Scripts to Set the rest of the Domain up when logged in
+New-Item -Path "c:\" -Name "Baselab" -ItemType "directory" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_DomainSetup.ps1" -OutFile "C:\BaselabS\baselab_StdSvrJoinDomain.ps1"
+#Setup and Partition Data Disk
+Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
