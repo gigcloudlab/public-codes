4afef0c5fc728f54f4e130663cfca285a2dfd9f3
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2022-12-20 15:56:37 +0800

    Update baselab_DCSetup.ps1
    Change new directory name to "baselab".

--- PowerShell/baselab_DCSetup.ps1
+++ PowerShell/baselab_DCSetup.ps1
@@ -9,9 +9,9 @@ choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
 #Download Scripts to Set the rest of the Domain up when logged in
-New-Item -Path "c:\" -Name "BaselabSetup" -ItemType "directory" -Force
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_DomainSetup.ps1" -OutFile "C:\BaselabSetup\baselab_DomainSetup.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_LabStructure.ps1" -OutFile "C:\BaselabSetup\baselab_LabStructure.ps1"
+New-Item -Path "c:\" -Name "Baselab" -ItemType "directory" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_DomainSetup.ps1" -OutFile "C:\Baselab\baselab_DomainSetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_LabStructure.ps1"
 #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
 #Allow Ping
