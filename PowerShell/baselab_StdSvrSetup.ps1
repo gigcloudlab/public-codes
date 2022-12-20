6c2e4a9654335ad4ee3cb8a2e9421b1614eddb82
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2022-12-20 16:03:25 +0800

    Update baselab_StdSvrSetup.ps1

--- PowerShell/baselab_StdSvrSetup.ps1
+++ PowerShell/baselab_StdSvrSetup.ps1
@@ -8,7 +8,7 @@ choco install notepadplusplus -y
 choco install winscp -y
 
 #Download Scripts to Set the rest of the Domain up when logged in
-New-Item -Path "c:\" -Name "Baselab" -ItemType "directory" -Force
+New-Item -Path "c:\" -Name "baselab" -ItemType "directory" -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_StdSvrJoinDomain.ps1" -OutFile "C:\Baselab\baselab_StdSvrJoinDomain.ps1"
 #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
