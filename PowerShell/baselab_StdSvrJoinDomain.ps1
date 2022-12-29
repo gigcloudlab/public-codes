b33cf4bf55199f5778993c3b162eb7912a7a603a
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2022-12-29 11:16:21 +0800

    Update baselab_StdSvrJoinDomain.ps1

--- PowerShell/baselab_StdSvrJoinDomain.ps1
+++ PowerShell/baselab_StdSvrJoinDomain.ps1
@@ -1,8 +1,8 @@
 #Setup Variables
-#Ensure it's the AD DNS (Check using PowerShell#- DNSClientServerAddress)
+#Update DNS server to the AD DNS (Check using PowerShell#- DNSClientServerAddress)
 #Userone password = "Admin2023"
 $DCName = "ad.lab"
 $LabComputers = "OU=Computers,OU=Lab,DC=ad,DC=lab"
 
 #Join Server to domain
-Add-Computer -credential ad\userone -DomainName $DCName -OUPath $LabComputers -restart
+Add-Computer -credential ad\u-domadm -DomainName $DCName -OUPath $LabComputers -restart
