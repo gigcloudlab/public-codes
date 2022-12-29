f6929e2202378a857cbeb4c1842ba831334ec710
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2022-12-29 10:37:40 +0800

    Create baselab_NewUsers.ps1

---
+++ PowerShell/baselab_NewUsers.ps1
@@ -0,0 +1,5 @@
+#Create users
+New-ADUser -name "userone" -SamAccountName "userone" -DisplayName "User One" -GivenName "One" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
+New-ADUser -name "usertwo" -SamAccountName "usertwo" -DisplayName "User Two" -GivenName "Two" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
+New-ADUser -name "u-entadm" -SamAccountName "u-entadm" -DisplayName "User Enterprise Admin" -GivenName "EntAdmin" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
+New-ADUser -name "u-domadm" -SamAccountName "u-domadm" -DisplayName "User Domain Admin" -GivenName "DomainAdmin" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
