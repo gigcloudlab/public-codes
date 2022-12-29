070eef7fc04c3b41fef47384c05868611981f7e0
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2022-12-29 13:09:27 +0800

    Update baselab_NewUsers.ps1

--- PowerShell/baselab_NewUsers.ps1
+++ PowerShell/baselab_NewUsers.ps1
@@ -3,3 +3,7 @@ New-ADUser -name "userone" -SamAccountName "userone" -DisplayName "User One" -Gi
 New-ADUser -name "usertwo" -SamAccountName "usertwo" -DisplayName "User Two" -GivenName "Two" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
 New-ADUser -name "u-entadm" -SamAccountName "u-entadm" -DisplayName "User Enterprise Admin" -GivenName "EntAdmin" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
 New-ADUser -name "u-domadm" -SamAccountName "u-domadm" -DisplayName "User Domain Admin" -GivenName "DomainAdmin" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
+
+#Add users to group
+Add-ADGroupMember -Identity "Enterprise Admins" -Members "u-entadm"
+Add-ADGroupMember -Identity "Domain Admins" -Members "u-domadm"
