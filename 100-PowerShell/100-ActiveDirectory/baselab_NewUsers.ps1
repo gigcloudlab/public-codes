8556d0c51b91db7817781609742756f4530759bb
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2023-01-08 15:14:57 +0800

    Update baselab_NewUsers.ps1

--- 100-PowerShell/100-ActiveDirectory/baselab_NewUsers.ps1
+++ 100-PowerShell/100-ActiveDirectory/baselab_NewUsers.ps1
@@ -3,6 +3,7 @@ New-ADUser -name "userone" -SamAccountName "userone" -DisplayName "User One" -Gi
 New-ADUser -name "usertwo" -SamAccountName "usertwo" -DisplayName "User Two" -GivenName "Two" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
 New-ADUser -name "uentadm" -SamAccountName "uentadm" -DisplayName "User EntAdmin" -GivenName "EntAdmin" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
 New-ADUser -name "udomadm" -SamAccountName "udomadm" -DisplayName "User DomAdmin" -GivenName "DomainAdmin" -Surname "User" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Users,OU=Lab,DC=ad,DC=lab'
+New-ADUser -name "saone" -SamAccountName "saone" -DisplayName "Service Account One" -GivenName "One" -Surname "Service Account" -PasswordNeverExpires $true -Enabled $true -accountPassword (ConvertTo-SecureString -AsPlainText "Admin2023" -Force) -Path 'OU=Service Accounts,OU=Lab,DC=ad,DC=lab'
 
 #Add users to group
 Add-ADGroupMember -Identity "Enterprise Admins" -Members "uentadm"
