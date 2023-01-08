6a3f416e453950f5c06866a26bc02c302dfb4095
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2023-01-08 14:50:27 +0800

    Update baselab_DCSetup.ps1

--- 100-PowerShell/100-ActiveDirectory/baselab_DCSetup.ps1
+++ 100-PowerShell/100-ActiveDirectory/baselab_DCSetup.ps1
@@ -10,9 +10,9 @@ choco install winscp -y
 choco install bginfo -y
 #Download Scripts to Set the rest of the Domain up when logged in
 New-Item -Path "c:\" -Name "Baselab" -ItemType "directory" -Force
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_DomainSetup.ps1" -OutFile "C:\Baselab\baselab_DomainSetup.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_LabStructure.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/PowerShell/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_NewUsers.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_DomainSetup.ps1" -OutFile "C:\Baselab\baselab_DomainSetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_LabStructure.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_NewUsers.ps1"
 #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
 #Allow Ping
