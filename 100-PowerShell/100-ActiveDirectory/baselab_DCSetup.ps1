fcd70e3cbf5538982e424343322ad380ded80c8a
gigcloudlab <120553966+gigcloudlab@users.noreply.github.com>
2023-01-08 14:52:57 +0800

    Update baselab_DCSetup.ps1

--- 100-PowerShell/100-ActiveDirectory/baselab_DCSetup.ps1
+++ 100-PowerShell/100-ActiveDirectory/baselab_DCSetup.ps1
@@ -12,7 +12,7 @@ choco install bginfo -y
 New-Item -Path "c:\" -Name "Baselab" -ItemType "directory" -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_DomainSetup.ps1" -OutFile "C:\Baselab\baselab_DomainSetup.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_LabStructure.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_LabStructure.ps1" -OutFile "C:\Baselab\baselab_NewUsers.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gigcloudlab/public-codes/main/100-PowerShell/100-ActiveDirectory/baselab_NewUsers.ps1" -OutFile "C:\Baselab\baselab_NewUsers.ps1"
 #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
 #Allow Ping
